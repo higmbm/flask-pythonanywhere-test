6b3cd595853f078b778006267d3bff82561dfa1f
higmbm <mbm@hig.se>
2026-02-10 16:59:00 +0100

    Added simple session example

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+app.secret_key = "Silmantra!"
 
 @app.route("/")
+
 def index():
-    return "Hello from Flask on PythonAnywhere!"
+    count = session.get("count", 0)
+    count += 1
+    session["count"] = count
+    return f"Du har laddat sidan {count} gånger i denna session."
 
 # OBS: kör INTE app.run() på PythonAnywhere
 if __name__ == "__main__":
